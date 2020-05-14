88e8b6aaa8cdec37c90be622ccd8d2cda66ed896
Alex Renoki <rennokki@gmail.com>
2020-05-14 16:16:23 +0300

    Added cronfile for log deleting.

--- .platform/hooks/postdeploy/create_cron_files.sh
+++ .platform/hooks/postdeploy/create_cron_files.sh
@@ -8,3 +8,9 @@
 echo "* * * * * root /usr/bin/php /var/app/current/artisan schedule:run 1>> /dev/null 2>&1" \
     | sudo tee /etc/cron.d/artisan_scheduler
 
+# In some cases, Laravel logs a lot of data in the storage/logs/laravel.log and it sometimes
+# might turn out into massive files that will restrict the filesystem.
+# Uncomment the following lines to enable a CRON that deletes the laravel.log file
+# every now and often.
+# echo "0 0 * * */7 root rm -rf /var/app/current/storage/logs/laravel.log 1>> /dev/null 2>&1" \
+#     | sudo tee log_deleter
