d37f555b859bee8387dc3c4e44eefd20982edd4a
Alex Couturon <infocontact.alex@gmail.com>
2021-07-01 11:36:41 +0200

    Update comment and only restart nginx

--- .platform/hooks/postdeploy/restart_services.sh
+++ .platform/hooks/postdeploy/restart_services.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-# This file will run restarts on specific services like PHP-FPM or NGINX.
-# During prebuild, some PHP extensions might be installed and a restart
-# might be needed.
-
-# We make sure to restart the services to take in consideration the new
-# custom config
-
-sudo systemctl restart php-fpm.service
+# This file will run restarts on specific services like NGINX.
+# During postdeploy, we make sure to restart the services to 
+#take in consideration the new custom config
 
 sudo systemctl restart nginx.service
