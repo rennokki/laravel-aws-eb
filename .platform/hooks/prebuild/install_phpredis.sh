60a652373009e2cf7ef7fcb7ff3be78b3af22b87
rennokki <rennokki@gmail.com>
2021-10-24 14:58:55 +0300

    Fixed php-redis for PHP 8.0+

--- .platform/hooks/prebuild/install_phpredis.sh
+++ .platform/hooks/prebuild/install_phpredis.sh
@@ -9,3 +9,9 @@
 # Enabled by default since it's latest Laravel version's default driver.
 
 sudo yum -y install php-redis
+
+# For PHP 8.0+, you can use the pecl command to install it. You may disable the previous command.
+
+# pecl install redis
+# sed -i -e '/extension="redis.so"/d' /etc/php.ini
+# echo 'extension="redis.so"' > /etc/php.d/41-redis.ini
